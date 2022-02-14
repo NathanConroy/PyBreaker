b5ad91e221a71f1d0ac0b838d46dbbd410a8b621
nconroy <nathanconroydev@gmail.com>
2022-02-14 13:27:06 -0600

    Prepare tests to test for failure in the wrapped function.

--- tests/test_pybrkr.py
+++ tests/test_pybrkr.py
@@ -4,19 +4,24 @@ Tests the pybrkr.py module.
 
 from src.pybrkr import PyBrkr
 
-TEST_RET_VAL = "hello world"
+SUCCESS_RESP = "success"
 
 
-def function():
+def function(succeed=True):
     """
-    A function to test out our circuit-breaker decorator on.
+    A function to test out our circuit-breaker on.
+
+    For now, we assume that any exception raised is what counts as an error. This
+    will change.
     """
-    return TEST_RET_VAL 
+    if succeed:
+        return SUCCESS_RESP
+    else:
+        raise Exception("Failed function call.")
 
 
 def test_decorator_init():
     """
     Merely tests that the decorator can wrap a function.
     """
-    assert PyBrkr(function)() == TEST_RET_VAL
-
+    assert PyBrkr(function)() == SUCCESS_RESP
