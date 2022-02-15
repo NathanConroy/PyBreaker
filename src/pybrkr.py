1283c79176a3c38202f2552fd77305eecce9fdcc
nconroy <nathanconroydev@gmail.com>
2022-02-15 12:07:05 -0600

    Redo PyBrkr class for use with @-decorator syntax.

--- src/pybrkr.py
+++ src/pybrkr.py
@@ -8,21 +8,19 @@ class PyBrkr():
     """
     def __init__(
         self,
-        fn: t.Callable,
         open_resp: t.Optional[t.Any] = None,
     ):
-        self._fn = fn
         self._open_resp = open_resp
 
-    def __call__(self, *args, **kwargs):
-        try:
-            # Success.
-            return self._fn(*args, **kwargs)
-        except Exception:
-            # Failure.
-            #
-            # Eventually clients will be able to specify
-            # what counts as a failure. But for now, all
-            # exceptions will be considered failures.
-            return self._open_resp
-
+    def __call__(self, fn: t.Callable):
+        def wrapper(*args, **kwargs):
+            try:
+                return fn(*args, **kwargs)
+            except Exception:
+                # Failure.
+                #
+                # Eventually clients will be able to specify
+                # what counts as a failure. But for now, only
+                # exceptions will be considered failures.
+                return self._open_resp
+        return wrapper
--- tests/test_pybrkr.py
+++ tests/test_pybrkr.py
@@ -8,7 +8,8 @@ SUCCESS_RESP = "success"
 OPEN_RESP = "open"
 
 
-def function(succeed: bool = True):
+@PyBrkr(open_resp=OPEN_RESP)
+def wrapped_fn(succeed: bool = True):
     """
     A function to test out our circuit-breaker on.
 
@@ -25,11 +26,11 @@ def test_success():
     """
     Merely tests that the decorator can wrap a function.
     """
-    assert PyBrkr(function)(succeed=True) == SUCCESS_RESP
+    assert wrapped_fn(succeed=True) == SUCCESS_RESP
 
 
 def test_failure():
     """
     Merely tests that the decorator can handle its wrapped function's failure.
     """
-    assert PyBrkr(function, open_resp=OPEN_RESP)(succeed=False) == OPEN_RESP
+    assert wrapped_fn(succeed=False) == OPEN_RESP
