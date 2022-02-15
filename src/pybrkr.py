69f0b79867fda690e6c1aebfa21a9d66da48b65e
nconroy <nathanconroydev@gmail.com>
2022-02-15 12:41:41 -0600

    Add TripCond class.

--- src/pybrkr.py
+++ src/pybrkr.py
@@ -1,5 +1,24 @@
-
 import typing as t
+
+
+class TripCond():
+    """
+    This is the base class for circuit breaker trip conditions.
+
+    In the future, this class will be abstract and PyBrkr will
+    require a child class.
+    """
+
+    def __init__(self):
+        self.max_failure = 0
+        self.fail_cnt = 0
+
+    def incr_fail(self):
+        self.fail_cnt += 1
+
+    def should_trip(self):
+        return self.fail_cnt >= self.max_failure
+
 
 class PyBrkr():
     """
@@ -10,6 +29,7 @@ class PyBrkr():
         self,
         open_resp: t.Optional[t.Any] = None,
     ):
+        self._trip_cond = TripCond()
         self._open_resp = open_resp
 
     def __call__(self, fn: t.Callable):
@@ -22,5 +42,9 @@ class PyBrkr():
                 # Eventually clients will be able to specify
                 # what counts as a failure. But for now, only
                 # exceptions will be considered failures.
-                return self._open_resp
+                self._trip_cond.incr_fail()
+                if self._trip_cond.should_trip():
+                    return self._open_resp
+                else:
+                    return self(fn)
         return wrapper
